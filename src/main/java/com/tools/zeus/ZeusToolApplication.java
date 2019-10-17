baf1895f132ef02b8a4e4778c3a4d203d799a4ad
wangbinzero <wangbbhtt@gmail.com>
2019-10-17 11:23:53 +0800

    init初始化

---
+++ src/main/java/com/tools/zeus/ZeusToolApplication.java
@@ -0,0 +1,12 @@
+package com.tools.zeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeusToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeusToolApplication.class, args);
+    }
+}
